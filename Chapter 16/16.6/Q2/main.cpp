74799a89c990f3cf9137aef9366f39f6f279c18d
Louis Nguyen <louis.nguyen550@gmail.com>
2026-05-28 11:24:18 -0400

    add 16.6 q2

---
+++ Chapter 16/16.6/Q2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+// Implement printArray() here
+template<typename T>
+void printArray(std::vector<T> &arr)
+{
+    for (std::size_t i{0}; i < arr.size(); i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::vector arr{4, 6, 7, 3, 8, 2, 1, 9};
+
+    printArray(arr); // use function template to print array
+
+    return 0;
+}
