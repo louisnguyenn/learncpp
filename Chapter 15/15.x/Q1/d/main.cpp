b0f687514f177facd8cbdb05b3570e171debf281
Louis Nguyen <louis.nguyen550@gmail.com>
2026-05-28 09:28:13 -0400

    add chapter 15, 15.x Q1d

---
+++ Chapter 15/15.x/Q1/d/main.cpp
@@ -0,0 +1,38 @@
+#include <string>
+
+class Monster
+{
+  public:
+    enum Type
+    {
+        dragon,
+        goblin,
+        ogre,
+        orc,
+        skeleton,
+        troll,
+        vampire,
+        zombie,
+        maxMonsterTypes
+    };
+
+  private:
+    Type m_type{};
+    std::string m_name;
+    std::string m_roar;
+    int m_hitPoints{};
+
+    // constructor
+  public:
+    Monster(Type type, std::string name, std::string roar, int hitPoints)
+        : m_type{type}, m_name{name}, m_roar{roar}, m_hitPoints{hitPoints}
+    {
+    }
+};
+
+int main()
+{
+    Monster skeleton{Monster::skeleton, "Bones", "*rattle*", 4};
+
+    return 0;
+}
