9f7a6b022b93c60f620eebfd8d48167085bd6d0c
Louis Nguyen <louis.nguyen550@gmail.com>
2026-05-29 15:39:19 -0400

    finish 16.x Q2 step 2

--- Chapter 16/16.x/Q2/main.cpp
+++ Chapter 16/16.x/Q2/main.cpp
@@ -1,4 +1,7 @@
+#include <cassert>
 #include <iostream>
+#include <string>
+#include <string_view>
 #include <vector>
 
 namespace Items
@@ -16,7 +19,7 @@ int countTotalItems(std::vector<int> &inventory)
 {
     int sum{0};
 
-    for (auto item: inventory)
+    for (auto item : inventory)
     {
         sum += item;
     }
@@ -24,11 +27,61 @@ int countTotalItems(std::vector<int> &inventory)
     return sum;
 }
 
+int countHealthPotion(std::vector<int> &inventory)
+{
+    return inventory[Items::health_potion];
+}
+
+int countTorches(std::vector<int> &inventory)
+{
+    return inventory[Items::torches];
+}
+
+int countArrows(std::vector<int> &inventory)
+{
+    return inventory[Items::arrows];
+}
+
 int main()
 {
     std::vector inventory{1, 5, 10};
+    assert(std::size(inventory) == Items::max_items);
 
-    std::cout << "You have " << countTotalItems(inventory) << " total items\n";
+    if (countHealthPotion(inventory) == 1)
+    {
+        std::cout << "You have " << countHealthPotion(inventory) << " health potion\n";
+    }
+    else
+    {
+        std::cout << "You have " << countHealthPotion(inventory) << " health potions\n";
+    }
+
+    if (countTorches(inventory) == 1)
+    {
+        std::cout << "You have " << countTorches(inventory) << " torch\n";
+    }
+    else
+    {
+        std::cout << "You have " << countTorches(inventory) << " torches\n";
+    }
+
+    if (countArrows(inventory) == 1)
+    {
+        std::cout << "You have " << countArrows(inventory) << " arrow\n";
+    }
+    else
+    {
+        std::cout << "You have " << countArrows(inventory) << " arrows\n";
+    }
+
+    if (countTotalItems(inventory) == 1)
+    {
+        std::cout << "You have " << countTotalItems(inventory) << " total item\n";
+    }
+    else
+    {
+        std::cout << "You have " << countTotalItems(inventory) << " total items\n";
+    }
 
     return 0;
 }
