7b925f805c8b91a44f6cce1aa06e042c19fc3311
Louis Nguyen <louis.nguyen550@gmail.com>
2026-05-31 17:24:28 -0400

    complete 21.2 q1 b.

--- Chapter 21/21.2/Q1/b/main.cpp
+++ Chapter 21/21.2/Q1/b/main.cpp
@@ -16,21 +16,46 @@ class Fraction
         std::cout << m_numerator << '/' << m_denominator << '\n';
     }
 
-    // friend Fraction operator/(Fraction &f1, Fraction &f2);
+    friend Fraction operator*(const Fraction &f, int val);
+    friend Fraction operator*(int val, const Fraction &f);
+    friend Fraction operator*(const Fraction &f1, const Fraction &f2);
 };
 
-// Fraction operator/(Fraction &f1, Fraction &f2)
-// {
+Fraction operator*(const Fraction &f, int val)
+{
+    return Fraction{f.m_numerator * val, f.m_denominator};
+}
 
-// }
+Fraction operator*(int val, const Fraction &f)
+{
+    // calls operator*(Fraction, int)
+    return f * val;
+}
+
+Fraction operator*(const Fraction &f1, const Fraction &f2)
+{
+    return Fraction{f1.m_numerator * f2.m_numerator, f1.m_denominator * f2.m_denominator};
+}
 
 int main()
 {
-    Fraction f1{1, 4};
+    Fraction f1{2, 5};
     f1.print();
 
-    Fraction f2{1, 2};
+    Fraction f2{3, 8};
     f2.print();
+
+    Fraction f3{f1 * f2};
+    f3.print();
+
+    Fraction f4{f1 * 2};
+    f4.print();
+
+    Fraction f5{2 * f2};
+    f5.print();
+
+    Fraction f6{Fraction{1, 2} * Fraction{2, 3} * Fraction{3, 4}};
+    f6.print();
 
     return 0;
 }
